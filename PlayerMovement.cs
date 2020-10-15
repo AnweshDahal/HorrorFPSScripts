a5e0d4886a57d7785293332bf266dcf04a232ecc
Anwesh Dahal <42203674+AnweshDahal@users.noreply.github.com>
2020-10-15 09:17:44 +0545

    Updates to movement script

--- PlayerMovement.cs
+++ PlayerMovement.cs
@@ -11,21 +11,27 @@ public class PlayerMovement : MonoBehaviour
     // vector to store movement on 3 dimensions forward, lateral, vertical
     private Vector3 movement;
 
+    // 2D vector to store input value
+    private Vector2 inputVector;
+
     [SerializeField]
-    private float moveSpeed = 10f;
+    private float moveSpeed = 10f; // The speed at which you would move horizontally on the ground
+
     [SerializeField]
-    private float gravity;
+    private float gravity = -9.85f; // The acceleration due to gravity
+
+    [SerializeField]
+    private float jumpHeight = 1.5f; // Your Jump Height
 
     // rotation of the head on z-axis
     private float pitch;
     [SerializeField]
-    private float pitchSpeed = 100f;
+    private float mouseSensitivity = 100f;
 
     // rotation of the body on y-axis
     private float yaw;
-    [SerializeField]
-    private float yawSpeed = 100f;
 
+    [SerializeField]
     private float verticalVelocity = 0f;
     // Start is called before the first frame update
     void Start()
@@ -40,27 +46,40 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
+        // CAMERA MOVEMENT
+        // ---------------
         // Time.deltaTime adjusts the yaw to the fps
-        yaw = Input.GetAxis("Mouse X") * yawSpeed * Time.deltaTime;
+        yaw = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
         transform.Rotate(new Vector3(0, yaw, 0)); // rotating the body on y axis
 
-        pitch -= Input.GetAxis("Mouse Y") * pitchSpeed * Time.deltaTime; // Increasing the pitch of the camera
+        pitch -= Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime; // Increasing the pitch of the camera
         // using '=' instead of '-=' produces a bug where your pitch resets on every frame
         pitch = Mathf.Clamp(pitch, -60f, 60f); // limiting the pitch to -60deg to +60deg
         Camera.main.transform.localRotation = Quaternion.Euler(pitch, 0, 0); // rotating the camera
 
+        // JUMPING
+        // --------
+        
+
         if (!cc.isGrounded)
         {
             verticalVelocity += gravity * Time.deltaTime; // downward acceleration during fall
-        } else if (cc.isGrounded)
-        {
-            verticalVelocity = 0; // stationary vertical velocity while grounded
+        } else if (cc.isGrounded){
+            verticalVelocity = -2f; // stationary vertical velocity while grounded
         }
 
-        float forwardMove = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime; // forward movement input
-        float lateralMove = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime; // lateral movement input
+        if (Input.GetButton("Jump") && cc.isGrounded)
+        {
+            verticalVelocity = 15f * Time.deltaTime;
+        }
 
-        movement = new Vector3(lateralMove, verticalVelocity, forwardMove); // setting the movement constraints
+        inputVector.x = Input.GetAxis("Vertical"); // forward movement input
+        inputVector.y = Input.GetAxis("Horizontal"); // lateral movement input
+
+        // Normalizing the vector to adjust the diagonal movement speed
+        inputVector = inputVector.normalized;
+
+        movement = new Vector3(inputVector.y * moveSpeed * Time.deltaTime, verticalVelocity, inputVector.x * moveSpeed * Time.deltaTime); // setting the movement constraints
         movement = transform.rotation * movement; // finding the resultant of rotation and movement
 
         cc.Move(movement); // moving the body
